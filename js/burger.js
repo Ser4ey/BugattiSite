87d56813ffef3b71797ac7006b60c1fc15f63ec4
Sergey <pristup2040@gmail.com>
2023-12-27 01:49:45 +1000

    upgrade burger.js

--- js/burger.js
+++ js/burger.js
@@ -3,3 +3,22 @@ document.querySelector('.burger').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('open');
 })
 
+
+function check_is_burger_active(){
+    const burger = document.querySelector(".burger")
+    return burger.classList.contains("active")
+}
+
+const elements = document.querySelectorAll(".nav__link");
+for (const element of elements) {
+    element.addEventListener('click', function () {
+        if (check_is_burger_active()) {
+            const burger = document.querySelector(".burger")
+            burger.classList.toggle('active');
+            document.querySelector('.nav').classList.toggle('open');
+        }
+    })
+
+
+}
+
